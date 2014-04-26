c0db94b71e159625e9963fffcbfa823fbfe23101
arvindsmith <arvindsmith@gmail.com>
2014-04-26 04:15:38 -0800

    Integrating Apache log 4j logging in the project

--- src/main/java/com/agilemaple/common/controller/HelloController.java
+++ src/main/java/com/agilemaple/common/controller/HelloController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -15,100 +16,108 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.agilemaple.common.dto.userDetails;
 import com.agilemaple.common.services.Register;
-import com.agilemaple.common.services.impl.RegisterImpl;
 
 @Controller
 @RequestMapping("/welcome")
 public class HelloController {
-	
+	// get log4j handler
+	private static final Logger logger = Logger
+			.getLogger(HelloController.class);
+
 	@Autowired
 	Register register;
-	
-	@RequestMapping(/*value="/registerpage",*/  method = RequestMethod.GET)
-	public String getRegisterpage(ModelMap model){
-         
-         return "home";
-         }
-/*	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
 
-		model.addAttribute("message", "Spring 3 MVC Hello World");
-		return "index";
+	@RequestMapping(/* value="/registerpage", */method = RequestMethod.GET)
+	public String getRegisterpage(ModelMap model) {
+		logger.debug("Inside getRegisterpage");
+		return "home";
+	}
+
+	/*
+	 * @RequestMapping(method = RequestMethod.GET) public String
+	 * printWelcome(ModelMap model) {
+	 * 
+	 * model.addAttribute("message", "Spring 3 MVC Hello World"); return
+	 * "index";
+	 * 
+	 * }
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String welcome(ModelMap model,
+			@RequestParam("username") String username,
+			@RequestParam("password") int password) {
+		logger.debug("Inside welcome");
+		if ((username.equals("gunny")) && (password == 123)) {
+			model.addAttribute("message", "Welcome Sir");
+			return "home";
+		} else {
+			model.addAttribute("message", "WRONG USERNAME @ PASSWORD");
+			return "err";
+		}
 
 	}
-	*/
-	@RequestMapping(value="/login",  method = RequestMethod.POST)
-	public String welcome(ModelMap model,@RequestParam("username") String username,@RequestParam("password") int password) {
-         System.out.println("inside console"); 
-		if((username.equals("gunny")) && (password == 123)){
-        	model.addAttribute("message","Welcome Sir");
-        	return "home";
-        }else{
-        	model.addAttribute("message","WRONG USERNAME @ PASSWORD");
-        	return "err";
-        }		
- 
-}
-	@RequestMapping(value="/register",  method = RequestMethod.POST)
-	public String register(ModelMap model,@RequestParam("firstName") String firstName ,
-			                @RequestParam("lastName") String lastName ,
-			                /*@RequestParam("sex") String sex*/ 
-			                @RequestParam("address") String address ,
-			                @RequestParam("province") String province ,
-			                @RequestParam("country") String country ,
-			                @RequestParam("postalCode") String postalCode ,
-			                @RequestParam("phoneNumber") String phoneNumber ,
-			                @RequestParam("email") String email ){
-         System.out.println("inside console"); 
-         List<String> lister = new ArrayList<String>();
-         lister.add(firstName);
-         //lister.add(sex);
-         lister.add(address);
-         lister.add(province);
-         lister.add(country);
-         lister.add(postalCode);
-         lister.add(phoneNumber);
-         lister.add(email);
-       //  RegisterImpl registral = new RegisterImpl();
-         boolean registerationStatus = register.register(lister);
-         if(registerationStatus){
-        	 model.addAttribute("userDetails",lister);
-         }
-         
-         return "home";
-       	
- 
-}
-	@RequestMapping(value="/myaccount",  method = RequestMethod.GET)
-	public String myaccount(ModelMap model,@RequestParam("id") Integer id) {
-//Register myacc = new RegisterImpl();
-Map<String,String> myAccountDetails = register.getUserDetails(id);
-model.addAttribute("myAccountDetails", myAccountDetails);
-System.out.println("hello");
-return "MyAccount";		
- 
-}
-	
-	
-	@RequestMapping(value="/myacc",  method = RequestMethod.GET)
-	public String myacc(ModelMap model,@RequestParam("id") Integer id) {
-		//Register myacc = new RegisterImpl();
-userDetails userDetail = register.getAccountDetails(id);
-System.out.println(userDetail.getAddress());
-model.addAttribute("userDetailObj",userDetail );
-return "MyAccount";		
- 
-}
-	
-	
-/*@RequestMapping(value="/registerpage",  method = RequestMethod.GET)
-	public String getRegisterpage(ModelMap model){
-         
-         return "Register";
-       	
- 
-}*/
 
-	} 
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String register(ModelMap model,
+			@RequestParam("firstName") String firstName,
+			@RequestParam("lastName") String lastName,
+			/* @RequestParam("sex") String sex */
+			@RequestParam("address") String address,
+			@RequestParam("province") String province,
+			@RequestParam("country") String country,
+			@RequestParam("postalCode") String postalCode,
+			@RequestParam("phoneNumber") String phoneNumber,
+			@RequestParam("email") String email) {
+		logger.debug("Inside register");
+		List<String> lister = new ArrayList<String>();
+		lister.add(firstName);
+		// lister.add(sex);
+		lister.add(address);
+		lister.add(province);
+		lister.add(country);
+		lister.add(postalCode);
+		lister.add(phoneNumber);
+		lister.add(email);
+		// RegisterImpl registral = new RegisterImpl();
+		boolean registerationStatus = register.register(lister);
+		if (registerationStatus) {
+			model.addAttribute("userDetails", lister);
+		}
+
+		return "home";
+
+	}
+
+	@RequestMapping(value = "/myaccount", method = RequestMethod.GET)
+	public String myaccount(ModelMap model, @RequestParam("id") Integer id) {
+		logger.debug("myaccount");
+		Map<String, String> myAccountDetails = register.getUserDetails(id);
+		model.addAttribute("myAccountDetails", myAccountDetails);
+		System.out.println("hello");
+		return "MyAccount";
+
+	}
+
+	@RequestMapping(value = "/myacc", method = RequestMethod.GET)
+	public String myacc(ModelMap model, @RequestParam("id") Integer id) {
+		logger.debug("myacc");
+		userDetails userDetail = register.getAccountDetails(id);
+		System.out.println(userDetail.getAddress());
+		model.addAttribute("userDetailObj", userDetail);
+		return "MyAccount";
+
+	}
+
+	/*
+	 * @RequestMapping(value="/registerpage", method = RequestMethod.GET) public
+	 * String getRegisterpage(ModelMap model){
+	 * 
+	 * return "Register";
+	 * 
+	 * 
+	 * }
+	 */
+
+} 
 	
 	
