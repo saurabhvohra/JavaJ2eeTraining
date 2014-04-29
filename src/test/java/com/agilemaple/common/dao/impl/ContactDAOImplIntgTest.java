99106837a84368da0e1bf60ed585183b7306720d
arvindsmith <arvindsmith@gmail.com>
2014-04-28 23:44:04 -0800

    Modifying maven pom xml to exclude integration test cases to run

--- src/test/java/com/agilemaple/common/dao/impl/ContactDAOImplIntgTest.java
+++ src/test/java/com/agilemaple/common/dao/impl/ContactDAOImplIntgTest.java
@@ -1,28 +1,37 @@
-package com.agilemaple.common.services.impl;
+package com.agilemaple.common.dao.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.agilemaple.common.dao.ContactDAO;
+import com.agilemaple.common.dao.RegisterDAO;
 import com.agilemaple.common.entity.Contact;
-import com.agilemaple.common.services.ContactService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"/test-dispatcher-servlet.xml"})
-public class ContactServiceImplTest {
+public class ContactDAOImplIntgTest {
 	
 	@Autowired
-	private ContactService contactService;
+	private ContactDAO contactDAO;
 	
 	@Test
+	@Transactional
 	public void registerTest(){
 		Contact contact = new Contact();
 		contact.setEmail("as@g.com");
-		contact.setFirstname("S");
-		contact.setLastname("V");
+		contact.setFirstname("Arvind");
+		contact.setLastname("Kashyap");
 		contact.setTelephone("5149636381");
-		contactService.addContact(contact);
+		contactDAO.addContact(contact);
 	}
 }
--- src/test/java/com/agilemaple/common/services/impl/ContactServiceImplIntgTest.java
+++ src/test/java/com/agilemaple/common/services/impl/ContactServiceImplIntgTest.java
@@ -1,37 +1,28 @@
-package com.agilemaple.common.dao.impl;
+package com.agilemaple.common.services.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import junit.framework.Assert;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.agilemaple.common.dao.ContactDAO;
-import com.agilemaple.common.dao.RegisterDAO;
 import com.agilemaple.common.entity.Contact;
+import com.agilemaple.common.services.ContactService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"/test-dispatcher-servlet.xml"})
-public class ContactDAOImplTest {
+public class ContactServiceImplIntgTest {
 	
 	@Autowired
-	private ContactDAO contactDAO;
+	private ContactService contactService;
 	
 	@Test
-	@Transactional
 	public void registerTest(){
 		Contact contact = new Contact();
 		contact.setEmail("as@g.com");
-		contact.setFirstname("Arvind");
-		contact.setLastname("Kashyap");
+		contact.setFirstname("S");
+		contact.setLastname("V");
 		contact.setTelephone("5149636381");
-		contactDAO.addContact(contact);
+		contactService.addContact(contact);
 	}
 }
